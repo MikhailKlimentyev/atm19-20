e1519f5eb48303386597913261d838b78e3d9a43
Mikhail Klimentyev <mikhail.klimentyev@gmail.com>
2019-10-02 23:05:55 +0300

    Add dao package

---
+++ new-year-sweet-gift/src/main/java/dao/impl/FileReader.java
@@ -0,0 +1,6 @@
+package dao.impl;
+
+import dao.api.IReadable;
+
+public class FileReader implements IReadable {
+}
---
+++ new-year-sweet-gift/src/main/java/dao/impl/FileWriter.java
@@ -0,0 +1,6 @@
+package dao.impl;
+
+import dao.api.IWritable;
+
+public class FileWriter implements IWritable {
+}
